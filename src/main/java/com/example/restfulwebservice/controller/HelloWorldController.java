5587546e66ce7b10457ecdca289b9137566b5ab0
Shivakant6387 <shivakant5875@gmail.com>
2023-05-16 16:01:11 +0530

    json format

--- src/main/java/com/example/restfulwebservice/controller/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/controller/HelloWorldController.java
@@ -1,8 +1,7 @@
 package com.example.restfulwebservice.controller;
 
+import com.example.restfulwebservice.helloWorld.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +9,9 @@ public class HelloWorldController {
    @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
     }
 }
